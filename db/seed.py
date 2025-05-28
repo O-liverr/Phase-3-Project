27835eddabbc0ca8bd7bfd433c3aa3d22bd3672b
O-liverr <oliekoech@gmail.com>
2025-05-29 00:39:02 +0300

    seed

--- db/seed.py
+++ db/seed.py
@@ -7,11 +7,3 @@ Base.metadata.create_all(bind=engine)
 session = Session()
 
 vehicle1 = Vehicle(name="Toyota Land Cruiser", price=150, image="landcruiser.jpg", seats=6, type="open-air")
-vehicle2 = Vehicle(name="Toyota Hiace", price=100, image="hiace.jpg", seats=8, type="van")
-
-destination1 = Destination(name="Maasai Mara", base_price=200, image="maasai-mara.jpg", distance=300)
-destination2 = Destination(name="Ol Pejeta", base_price=150, image="ol-pejeta.jpg", distance=250)
-
-session.add_all([vehicle1, vehicle2, destination1, destination2])
-session.commit()
-session.close()
