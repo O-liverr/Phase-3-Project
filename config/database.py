3fe479845b08acf3b23e1c421d8f19b7cd48c481
O-liverr <oliekoech@gmail.com>
2025-05-29 00:29:39 +0300

    database

--- config/database.py
+++ config/database.py
@@ -2,6 +2,3 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///safari.db')
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
